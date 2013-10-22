0b5a6130a3e7a3124188fa9718697b68ff91bfd5
Leonardo Azopardo <lazopard@gmail.com>
2013-10-22 13:27:14 -0400

    handles commandline arguments

--- lab7/huffman.c
+++ lab7/huffman.c
@@ -1,4 +1,4 @@
-#define INPUTFILE "-i"
+#define INPUTFILE "-ib"
 #define OUTPUTFILE "-o"
 #define CODEFILE "--code"
 
@@ -7,4 +7,44 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
+	int i, decode;
+	int finput, foutput;
+	const char inputFile, outputFile, codeFile;
+	for(i =0; i < argc; i++) { //deal with command-line arguments
+		if (!strncmp(argv[i], INPUTFILE,2)) {
+			if (!strcmp(argv[i], INPUTFILE))  //input file is binary
+				decode = 1;
+			else 
+				decode = 0;
+			inputFile = argv[i+1];
+			i++;
+		}
 
+		else if (!strcmp(argv[i], OUTPUTFILE)) {
+			outputFile = argv[i+1];	
+			i++;
+		}
+
+		else if (!strcmp(argv[i], CODEFILE)) {
+			codeFile = argv[i+1];
+			i++;
+		}
+		else { //unknown command-line argument
+			printf("Invalid argument");
+			return 1;
+		}
+	}
+
+	finput = open(inputFile, O_RDONLY);
+	foutput = open(outputFile, O_WRONLY);
+
+	if (!decode) {
+		
+	}
+
+	else { //decode
+
+	}
+	close(finput);
+	close(foutput);
+}
