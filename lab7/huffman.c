c4853bc76deb06e08028d8eaad01dd629b087ace
Leonardo Azopardo <lazopard@gmail.com>
2013-10-22 23:33:02 -0400

    huffman handles input

--- lab7/huffman.c
+++ lab7/huffman.c
@@ -6,9 +6,11 @@
 #include <string.h>
 #include <stdio.h>
 
+unsigned char charToCode(char c, FILE *code);
+
 int main(int argc, char **argv) {
-	int i, decode;
-	char *inputFile, *outputFile, *codeFile;
+	int i, decode, c, byteCount, charCode;
+	const char *inputFile, *outputFile, *codeFile;
 	for(i = 1; i < argc; i++) { //deal with command-line arguments
 		if (!strncmp(argv[i], INPUTFILE,2)) {
 			if (!strcmp(argv[i], INPUTFILE))  //input file is binary
@@ -34,9 +36,47 @@ int main(int argc, char **argv) {
 		}
 	}
 
-	if (!decode) {
+	FILE *input = fopen(inputFile, "r");
+	if (input == NULL) {
+		printf("File %s does not exist.\n", inputFile);
+		return 1;
+	}
+	FILE *output = fopen(outputFile, "w");
+	if (output == NULL) {
+		printf("File %s does not exist.\n", outputFile);
+		return 1;
+	}
+	FILE *code = fopen(outputFile, "r");
+	if (code == NULL) {
+		printf("File %s does not exist.\n", codeFile);
+		return 1;
 	}
 
-	else { //decode
+	//encode input
+	if (!decode) {
+		while((c = fgetc(input)) != EOF) {
+			charCode = charToCode(c,code);
+			fwrite(&charCode,sizeof(char),1,output);
+		}
 	}
+	//decode input
+	else {
+		
+	}
+
+	fclose(input);
+	fclose(output);
+	fclose(code);
+	return 0;
+}
+
+unsigned char charToCode(char c, FILE *code) {
+	char temp;
+	while((temp = fgetc(code)) != EOF) {
+		if (temp == c) {
+			fgetc(code);
+			return fgetc(code);
+		}
+	}
+	return 1;
 }
