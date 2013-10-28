f7a059a75002fb18be59f18146549f394b296194
VirtualBoxImages.com <adminuser@adminuser-VirtualBox-073n.(none)>
2013-10-28 07:25:17 -0400

    builds charbyte array

--- lab7/huffman.c
+++ lab7/huffman.c
@@ -12,132 +12,148 @@ struct charByte {
 				char c;
 };
 
-unsigned char charToCode(char c, FILE *code);
-
-void initBuffer(char **buffer);
+void printCharByteArray(struct charByte[], int size);
 
 int main(int argc, char **argv) {
-	int i, charCode, decode;
-	const char *inputFile, *outputFile, *codeFile;
-  char *buffer = (char *) malloc(sizeof(char)*BUFFERSIZE);
-  if (argc < 7) {
-				printf("not enough arguments\n");
-				return 1;
-  }
-  
-	for(i = 1; i < argc; i++) { //deal with command-line arguments
-		if (!strncmp(argv[i], INPUTFILE,2)) {
-			if (!strcmp(argv[i], INPUTFILE))  //input file is binary
-				decode = 1;
-			inputFile = argv[i+1];
-			i++;
-		}
-
-		else if (!strcmp(argv[i], OUTPUTFILE)) {
-			outputFile = argv[i+1];	
-			i++;
-		}
-
-		else if (!strcmp(argv[i], CODEFILE)) {
-			codeFile = argv[i+1];
-			i++;
-		}
-		else { //unknown command-line argument
-			printf("Invalid argument");
-			return 1;
-		}
-	}
-
-	FILE *input = fopen(inputFile, "r");
-	if (input == NULL) {
-		printf("File %s does not exist.\n", inputFile);
-		return 1;
-	}
-  FILE *output;
-  if (!decode) {
-				*output = fopen(outputFile, "bw+");
-  }
-  else {
-				*output = fopen(outputFile, "w");
-  }
-	if (output == NULL) {
-		printf("File %s does not exist.\n", outputFile);
-		return 1;
-	}
-	FILE *code = fopen(outputFile, "r");
-	if (code == NULL) {
-		printf("File %s does not exist.\n", codeFile);
-		return 1;
-	}
-
-  int c, charByteArraySize;
-  c = 0;
-  charByteArraySize = 0;
-
-  while((c = fgetc(code)) != EOF) { 
-				if (c == '\n') {
-								charByteArraySize++;
+				int i, charCode, decode;
+				i=charCode=decode=0;
+				const char *inputFile, *outputFile, *codeFile;
+				inputFile=outputFile=codeFile=NULL;
+				char *buffer = (char *) malloc(sizeof(char)*BUFFERSIZE);
+				if (argc < 7) {
+								printf("not enough arguments\n");
+								return 1;
 				}
-  }
+				for(i = 1; i < argc; i++) { //deal with command-line arguments
+								if (!strncmp(argv[i], INPUTFILE,2)) {
+												if (!strcmp(argv[i], INPUTFILE)) { //input is binary
+																decode = 1;
+												}
+												inputFile = argv[i+1];
+												i++;
+								}
 
-  rewind(code);  
+								else if (!strcmp(argv[i], OUTPUTFILE)) {
+												outputFile = argv[i+1];	
+												i++;
+								}
 
-  struct charByte charByteArray[charByteArraySize];
-
-  //populate charByteArray
-
-  i = 0;
-  while((c = fgetc(code)) != EOF) {
-				if (c == '=') {
-						printf("%d\n",ungetc(c,code));
-						charByteArray[i].c = ungetc(c,code);
-								//previous of c is char
-								//after c is binary
-						printf("%c\n",fgetc(code));
-						charByteArray[i].byte = fgetc(code);
-				}
-  }
-
-  int sizeOfWrite, j;
-  j =0;
-
-	//encode input
-	if (!decode) {
-				while((c = fgetc(input)) != EOF) {
-								for(i = 0; i < charByteArraySize; i++) {
-												if (c == charByteArray[i].c) {
-																buffer[j] = charByteArray[i].byte;
-																j++;
-												}	
+								else if (!strcmp(argv[i], CODEFILE)) {
+												codeFile = argv[i+1];
+												i++;
+								}
+								else { //unknown command-line argument
+												printf("Invalid argument");
+												return 1;
 								}
 				}
-				//deal with padding 0s
-				//add them to the buffer
-				fwrite(buffer, sizeof(char), j, output);
-	}
 
-	//decode input
+				FILE *input;
+				if (!decode) {
+								input = fopen(inputFile, "r");
+				}
+				else {
+								input = fopen(inputFile, "rb");
+				}
+				if (input == NULL) {
+								printf("File %s does not exist.\n", inputFile);
+								return 1;
+				}
+				FILE *output;
+				if (!decode) {
+								output = fopen(outputFile, "wb+");
+				}
+				else {
+								output = fopen(outputFile, "w");
+				}
+				if (output == NULL) {
+								printf("File %s does not exist.\n", outputFile);
+								return 1;
+				}
+				FILE *code = fopen(outputFile, "r");
+				if (code == NULL) {
+								printf("File %s does not exist.\n", codeFile);
+								return 1;
+				}
 
-  /* facts about decoding:
-				* ends with a period
-				* after this last period, there are only padding 0s
-				* So if we find a period either part or all of which lies in the last byte,
-				  and after this period all the remaining bits are 0, then this period has to be
-				  the last period
-        * You can assume that the period will not have code consisting f only 0s, or code that is 
-				  longer than a byte
-  */
+				int c, charByteArraySize;
 
-	else {
-				//using the | operator might work			
-				//| every byte with 0000000 until it doesn't change
-				//then those have to be the padding 0s
-	}
+				while((c = fgetc(code)) != EOF) { 
+								if (c == '\n') {
+												charByteArraySize++;
+								}
+				}
+				charByteArraySize++;
 
-  free(buffer);
-	fclose(input);
-	fclose(output);
-	fclose(code);
-	return 0;
+				rewind(code);  
+
+				struct charByte charByteArray[charByteArraySize];
+
+				//populate charByteArray
+
+				i = 0, c = 0;
+				while((c = fgetc(code)) != EOF) {
+								if (c == '=') {
+												charByteArray[i].c = ungetc(c,code);
+												//previous of c is char
+												//after c is binary
+												charByteArray[i].byte = fgetc(code);
+												i++;
+								}
+				}
+
+				int sizeOfWrite, j;
+				j = 0;
+
+				//encode input
+				if (!decode) {
+								while((c = fgetc(input)) != EOF) {
+												for(i = 0; i < charByteArraySize; i++) {
+																if (c == charByteArray[i].c) {
+																				buffer[j] = charByteArray[i].byte;
+																				j++;
+																}	
+												}
+								}
+								//deal with padding 0s
+								unsigned char padding = 00000000;
+								buffer[j + 1] = padding;
+								fwrite(buffer, sizeof(char), j, output);
+				}
+
+				//decode input
+
+				/* facts about decoding:
+				 * ends with a period
+				 * after this last period, there are only padding 0s
+				 * So if we find a period either part or all of which lies in the last byte,
+				 and after this period all the remaining bits are 0, then this period has to be
+				 the last period
+				 * You can assume that the period will not have code consisting f only 0s, or code that is 
+				 longer than a byte
+				 */
+
+				else {
+								//using the | operator might work			
+								//| every byte with 0000000 until it doesn't change
+								//then those have to be the padding 0s
+				}
+
+				free(buffer);
+				fclose(input);
+				fclose(output);
+				fclose(code);
+				buffer = NULL;
+				return 0;
+}
+
+
+void printCharByteArray(struct charByte charByteArray[], int size) {
+				int i;
+				for(i = 0; i < size; i++) {
+								printf("charByteArray[i].c is %c, charByteArray[i].byte is %u\n", charByteArray[i].c,
+																charByteArray[i].byte);
+				}
+				return;
 }
 
