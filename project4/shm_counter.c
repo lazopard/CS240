69540dcce7395b4ea6093d8a89eed4f29faabada
Leonardo Azopardo <lazopard@gmail.com>
2013-11-27 21:28:41 -0500

    Added everything but the counting itself

--- project4/shm_counter.c
+++ project4/shm_counter.c
@@ -47,24 +47,22 @@ void destroy_sharedmem() {
 	 */
 	shmem_checker();
 
-	/**
-	 * TODO: Write your code to clean all shared memory you created here.
-	 */
+	if (shmdt(shared_mem) == -1) { /*detach shared_mem*/
+		perror("shmdt failed...\n");
+	}
 
-} //destroy_sharedmem()
-
-/**
- * TODO: Create whatever functions you need here.
- */
-
-/**
- * TODO: Complete the main function.
- * Be sure to set keys_cnt, keywords, shm_id, and shared_mem to their actual values. 
- */
+	struct shmid_ds shmid_struct;
+	if (shmctl(shm_id, IPC_RMID, &shmid_struct) == -1) { /*remove memory id*/
+		perror("shmctl failed...\n");
+		exit(EXIT_FAILURE);
+	}
+}
 
 int isValidC(char c);
 
 int fillKeyArray(char ***keywords, FILE *keywordFile);
+
+void printProcKeyCount(struct proc_key_count keyCount, char **keywords, FILE *outputStream);
 
 int main(int argc, char **argv) {
 
@@ -101,35 +99,103 @@ int main(int argc, char **argv) {
 
 	/*Args processing end*/
 
+	/*Read from input*/
+
+	FILE *inputFile = fopen(input, "r");
+	fseek(inputFile, 0, SEEK_END);
+	int fileSize = ftell(inputFile);
+	rewind(inputFile);
+	char *buffer = malloc(sizeof(char)*size + 1);
+	fread(buffer, sizeof(char), fileSize, inputFile);
+	fclose(inputFile);
 
 	/*Initialize keyword array*/
 
-	keywords = malloc(sizeof(char *)*MAXKEYSIZE);
 	FILE *keywordFile = fopen(keyword,"r");
+	keywords = malloc(sizeof(char *)*MAXKEYSIZE);
 	keys_cnt = fillKeyArray(&keywords, keywordFile);
 	fclose(keywordFile);
 
-	/*Initialize file streams*/
+	/*Initialize multi-process variables and shared memory*/
 
-	FILE *inputFile = fopen(input, "r");
-	FILE *outputFile = fopen(output, "w");
+	shm_id = SHMGET(IPC_PRIVATE, sizeof(struct proc_key_count *),IPC_CREAT | IPC_EXCL | 0666);
+	if (shm_id == -1) {
+		perror("SHMGET failed\n");
+		return 1;
+	}
 
-	/*Count words*/
+	shared_mem = (struct proc_key_count **) shmat(shm_id,NULL,0);
+	if (shared_mem == NULL) {
+		perror("SHMAT failed\n");
+		return 1;
+	}
 
-	/*free memory, close streams*/
+	int semid = SEMGET(IPC_PRIVATE, MAX_PROCS, IPC_CREAT | IPC_EXCL | 0666);
 
-	fclose(inputFile);
+	if (semid == -1) {
+		perror("semget failed...\n");
+		return 1;
+	}
+
+	/*Counting starts */
+
+	for(i = 0; i < MAX_PROCS;i++) {
+
+		cCount=childCount=0;
+
+		if (pipe(pipe_fds) == -1) {
+			perror("pipe failed...\n");
+			exit(EXIT_FAILURE);
+		}
+
+		cpid = fork();
+
+		if (cpid < 0) { /*if ford failed*/
+			perror("fork failed...\n");
+			return 1;
+		}
+		else if (cpid == 0) { /*child process*/
+			close(pipe_fds[0]);
+			char *tempString = malloc(sizeof(char)*fileSizePerChild);
+			fseek(inputFile,i*fileSizePerChild, SEEK_SET);
+			fread(tempString, sizeof(char), fileSizePerChild, inputFile);
+			childCount = countWords(tempString, keyword);
+			write(pipe_fds[1], &childCount, sizeof(int));
+			free(tempString);
+			_exit(0);
+		}
+		else { /*parent process*/
+		}
+	}	
+
+	/*Counting ends*/
+
+	/*Write results to ouputFile*/
+
+	FILE *ouputFile = fopen(output);
+
+	for(i = 0; i < keys_cnt; i++) {
+		printProcKeyCount(*(shared_mem[0]),keywords, outputFile);
+	}
 	fclose(outputFile);
+
+	/*free memory, destroy shared mem...*/
+
+	free(buffer);
 
 	destroy_sharedmem();
 
 	return 0;
 }
 
+/*checks if C is a valid character for a keyword as specified in the handout*/
+
 int isValidC(char c) {
 	return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c == ',')
 		|| (c =='.') || (c == ';') || (c == '!') ;
 }
+
+/*checks if the line contains a valid keyword as specified in the handout*/
 
 int isValidLine(char *string) {
 	int i;
@@ -143,6 +209,8 @@ int isValidLine(char *string) {
 	return 1;
 }
 
+/*Fills the empty char* array KEYWORDS with the keywords in KEYWORDFILE*/
+
 int fillKeyArray(char ***keywords, FILE *keywordFile) {
 	int i = 0;
 	char *string = malloc(sizeof(char)*KEYLENGTH);
@@ -155,8 +223,10 @@ int fillKeyArray(char ***keywords, FILE *keywordFile) {
 		memset(string, '\0', KEYLENGTH);
 	}
 	free(string);
-	return i - 1;
+	return i;
 }  
+
+/*checks if C is a delimiter as specified in the handout*/
 
 int isDelimiter(char c) {
 	return (c == ' ' || c == '\t' || c == '\n');
@@ -190,4 +260,10 @@ int countWords(char *string, const char *keyword) {
 			i++;
 	}
 	return count;
+}
+
+/*prints to outputStream with specified format*/
+
+void printProcKeyCount(struct proc_key_count keyCount, char **keywords, FILE *outputStream) {
+	fprintf(outputStream, "%s: %d\n",keywords[key_id], keyCount.freq);
 }
